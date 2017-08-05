e8b24fe7548d2993a334114e5c6e9b0942dc7ac9
wangtao <t.wang@avid.ly>
2017-08-05 17:32:09 +0800

    2025 demo

--- app/src/main/java/com/avidly/adsdk/demo/MyApplication.java
+++ app/src/main/java/com/avidly/adsdk/demo/MyApplication.java
@@ -1,6 +1,6 @@
 package com.avidly.adsdk.demo;
 
-import android.support.multidex.MultiDexApplication;
+import android.app.Application;
 
 import com.avidly.ads.AvidlyAdsSdk;
 
@@ -8,7 +8,7 @@ import com.avidly.ads.AvidlyAdsSdk;
  * Created by Holaverse on 2017/3/30.
  */
 
-public class MyApplication extends MultiDexApplication {
+public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
